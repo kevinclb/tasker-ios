316839f8a5c2f6936ce61ac591684346f048b080
kevinbabou <kevincbabou@gmail.com>
2022-02-02 14:08:15 -0800

    create blank files + directories for all of our pages

---
+++ tasker/tasker/ViewControllers/LoggedOutViewController/LoggedOutViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoggedOutViewController.swift
+//  tasker
+//
+//  Created by Kevin Babou on 2/2/22.
+//
+
+import UIKit
+
+class LoggedOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //this should be the root view controller
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
