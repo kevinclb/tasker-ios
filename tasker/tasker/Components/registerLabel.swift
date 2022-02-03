3a89f0dfb443ddeb6a462771688f70117101ee93
kevinbabou <kevincbabou@gmail.com>
2022-02-02 16:53:17 -0800

    creating reusable registerButton component

---
+++ tasker/tasker/Components/registerLabel.swift
@@ -0,0 +1,20 @@
+//
+//  registerLabel.swift
+//  tasker
+//
+//  Created by Kevin Babou on 2/2/22.
+//
+
+import UIKit
+
+class registerLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
