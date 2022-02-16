8d076b83fdc5d47ff4789c77c22a82c72c0418e1
j3ffr3yv <44720313+j3ffr3yv@users.noreply.github.com>
2022-02-15 23:15:57 -0800

    CollectionViewCells

--- tasker/tasker/ViewControllers/EmployeeExplorePageViewController/EmployeeExplorePageViewController.swift
+++ tasker/tasker/ViewControllers/EmployeeExplorePageViewController/EmployeeExplorePageViewController.swift
@@ -13,35 +13,50 @@ class EmployeeExplorePageViewController: UIViewController{
     
     @IBOutlet var collectionView: UICollectionView!
     
+    let taskCollectionViewCellId = "MyCollectionViewCell"
+    
+    var tasks = [task]()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        collectionView.register(MyCollectionViewCell.self/*nib()*/, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        let nibCell = UINib(nibName: taskCollectionViewCellId, bundle: nil)
+        collectionView.register(nibCell, forCellWithReuseIdentifier: taskCollectionViewCellId)
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+        MyCollectionViewCell.awakeFromNib()
+        for _ in 1...25{
+            let task = task()
+            task.name = "Name"
+            task.city = "City"
+            task.taskInfo = "Task Description will go here"
+            task.price = "Price"
+            task.category = "Category"
+            tasks.append(task)
+        }
+        collectionView.reloadData()
     }
 }
 
-extension EmployeeExplorePageViewController: UICollectionViewDelegate{
+extension EmployeeExplorePageViewController : UICollectionViewDelegate, UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-     collectionView.deselectItem(at: indexPath, animated: true)
-    print("You tapped me")
-    }
-}
-
-extension EmployeeExplorePageViewController: UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return tasks.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
-//        cell.configure(with: UIImage(systemName: "house")!)
-//        cell.configure(with: UILabel(frame: ))
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: taskCollectionViewCellId, for: indexPath) as! MyCollectionViewCell
+        let task = tasks[indexPath.row]
+        cell.lbName.text = task.name!
+        cell.lbCity.text = task.city!
+        cell.lbDesc.text = task.taskInfo!
+        cell.lbPrice.text = task.price!
+        cell.lbCategory.text = task.category!
+        
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            let task = tasks[indexPath.row]
+            print("\(indexPath.row) - \(task.name!)")
+        }
 }
-
