582bf0e6de8090b396cc3898f67886e03074cf40
Issa Issa <iissa3339@gmail.com>
2022-02-15 20:50:47 -0800

    Slide Out menu works now
    just need to try to implement the swiping so the menu works with swiping and not only the hamburger button

--- tasker/tasker/ViewControllers/HomeExplorePageViewController/HomeExplorePageViewController.swift
+++ tasker/tasker/ViewControllers/HomeExplorePageViewController/HomeExplorePageViewController.swift
@@ -10,21 +10,47 @@ import UIKit
 class HomeExplorePageViewController: UIViewController {
 
     @IBOutlet weak var exploreButton: UIButton!
+    
+    
+    @IBOutlet weak var menuView: UIView!
+    
+    @IBOutlet weak var menuTrailing: NSLayoutConstraint!
+    @IBOutlet weak var menuLeading: NSLayoutConstraint!
+    var menuOut = false
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func menuTapped(_ sender: Any) {
+        /*
+        let vc = MenuViewController()
+        //self.present(vc, animated: true, completion: nil)
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        present(vc, animated: false, completion: nil)
+        */
+        if(menuOut == false){
+            setView(view: menuView, hidden: false)
+            menuLeading.constant = 300
+            menuTrailing.constant = -300
+            menuOut = true
+        }
+        else{
+            setView(view: menuView, hidden: true)
+            menuLeading.constant = 0
+            menuTrailing.constant = 0
+            menuOut = false
+        }
+        
     }
-    */
-
+    func setView(view: UIView, hidden: Bool) {
+        UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            view.isHidden = hidden
+        })
+    }
 }
