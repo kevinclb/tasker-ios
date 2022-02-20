bef5a9d93c9f3c5be033d0aff39500038c3ffbe9
Issa Issa <iissa3339@gmail.com>
2022-02-20 11:32:56 -0800

    Menu ScrollView works

--- tasker/tasker/ViewControllers/HomeExplorePageViewController/HomeExplorePageViewController.swift
+++ tasker/tasker/ViewControllers/HomeExplorePageViewController/HomeExplorePageViewController.swift
@@ -12,44 +12,42 @@ class HomeExplorePageViewController: UIViewController {
     @IBOutlet weak var exploreButton: UIButton!
     
     
+    
+    @IBOutlet weak var menuScroll: UIScrollView!
+    
     @IBOutlet weak var menuView: UIView!
     
-    @IBOutlet weak var menuTrailing: NSLayoutConstraint!
     @IBOutlet weak var menuLeading: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuTrailing: NSLayoutConstraint!
+    
+    
     var menuOut = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuScroll.bounces = false
+        menuScroll.showsVerticalScrollIndicator = false
+        menuScroll.showsHorizontalScrollIndicator = false
         // Do any additional setup after loading the view.
     }
 
     @IBAction func menuTapped(_ sender: Any) {
-        /*
-        let vc = MenuViewController()
-        //self.present(vc, animated: true, completion: nil)
-        let transition = CATransition()
-        transition.duration = 0.5
-        transition.type = CATransitionType.push
-        transition.subtype = CATransitionSubtype.fromLeft
-        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
-        view.window!.layer.add(transition, forKey: kCATransition)
-        present(vc, animated: false, completion: nil)
-        */
         if(menuOut == false){
-            setView(view: menuView, hidden: false)
-            menuLeading.constant = 300
-            menuTrailing.constant = -300
+            setView(view: menuScroll, hidden: false)
+            menuLeading.constant = 0
+            menuTrailing.constant = 0
             menuOut = true
         }
         else{
-            setView(view: menuView, hidden: true)
-            menuLeading.constant = 0
-            menuTrailing.constant = 0
+            setView(view: menuScroll, hidden: true)
+            menuLeading.constant = -300
+            menuTrailing.constant = 300
             menuOut = false
         }
         
     }
-    func setView(view: UIView, hidden: Bool) {
-        UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+    func setView(view: UIScrollView, hidden: Bool) {
+        UIScrollView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
             view.isHidden = hidden
         })
     }
