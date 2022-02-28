97f011ea068cebd26a7a83927e8648d69b647fdc
Issa Issa <iissa3339@gmail.com>
2022-02-28 11:34:37 -0800

    Settings page UI done
    Finished UI for settings page and also implemented the backend for pressing the edit profile button, figured out how to display the edit profile viewcontroller in full screen and from right to left

--- tasker/tasker/ViewControllers/SettingsPageViewController/SettingsPageViewController.swift
+++ tasker/tasker/ViewControllers/SettingsPageViewController/SettingsPageViewController.swift
@@ -14,8 +14,6 @@ class SettingsPageViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-
     /*
     // MARK: - Navigation
 
@@ -25,5 +23,33 @@ class SettingsPageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func accountSettingsTapped(_ sender: Any) {
+        
+    }
+    @IBAction func editProfileTapped(_ sender: Any) {
+        // this code here is to present from right to left instead of bottom to top
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        
+        // this code here is to make the viewcontroller we're presenting and make it show full screen then present it
+        let newViewController = EditProfileViewController()
+        newViewController.modalPresentationStyle = .fullScreen
+        // the app will automatically know how to animate the presentation, it will use the transition we made above on its own so that's why we set animated to false
+        self.present(newViewController, animated: false, completion: nil)
+    }
+    @IBAction func paymentInfoTapped(_ sender: Any) {
+        
+    }
+    @IBAction func appNotifTapped(_ sender: Any) {
+        
+    }
+    @IBAction func helpTapped(_ sender: Any) {
+        
+    }
+    @IBAction func logoutTapped(_ sender: Any) {
+    }
 }
