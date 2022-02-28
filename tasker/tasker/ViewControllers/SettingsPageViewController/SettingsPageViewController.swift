a43b0581c61d3e3ed71511ccf414c884983876e8
Issa Issa <iissa3339@gmail.com>
2022-02-28 12:29:36 -0800

    made the navigate function reusable in settings
    navigate function takes us from one viewcontroller to the other

--- tasker/tasker/ViewControllers/SettingsPageViewController/SettingsPageViewController.swift
+++ tasker/tasker/ViewControllers/SettingsPageViewController/SettingsPageViewController.swift
@@ -19,19 +19,7 @@ class SettingsPageViewController: UIViewController {
         
     }
     @IBAction func editProfileTapped(_ sender: Any) {
-        // this code here is to present from right to left instead of bottom to top
-        let transition = CATransition()
-        transition.duration = 0.5
-        transition.type = CATransitionType.push
-        transition.subtype = CATransitionSubtype.fromRight
-        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
-        view.window!.layer.add(transition, forKey: kCATransition)
-        
-        // this code here is to make the viewcontroller we're presenting and make it show full screen then present it
-        let newViewController = EditProfileViewController()
-        newViewController.modalPresentationStyle = .fullScreen
-        // the app will automatically know how to animate the presentation, it will use the transition we made above on its own so that's why we set animated to false
-        self.present(newViewController, animated: false, completion: nil)
+        navigateTo(newViewController: EditProfileViewController(), transitionFrom: .fromRight)
     }
     @IBAction func paymentInfoTapped(_ sender: Any) {
         
@@ -43,5 +31,25 @@ class SettingsPageViewController: UIViewController {
         
     }
     @IBAction func logoutTapped(_ sender: Any) {
+        
+    }
+    @IBAction func backButtonTapped(_ sender: Any) {
+
+    }
+    
+    func navigateTo(newViewController:UIViewController, transitionFrom:CATransitionSubtype){
+        // this code here is to present from right to left instead of bottom to top
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = transitionFrom
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        
+        // this code here is to make the viewcontroller we're presenting and make it show full screen then present it
+        let newVC = newViewController
+        newVC.modalPresentationStyle = .fullScreen
+        // the app will automatically know how to animate the presentation, it will use the transition we made above on its own so that's why we set animated to false
+        self.present(newVC, animated: false, completion: nil)
     }
 }
