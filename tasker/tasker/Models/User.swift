c62812761b57543d018e44b0065466346954d03e
kevinbabou <kevincbabou@gmail.com>
2022-03-17 22:50:10 -0700

    user model conforms to firebase user model

--- tasker/tasker/Models/User.swift
+++ tasker/tasker/Models/User.swift
@@ -10,7 +10,8 @@ import Foundation
 struct User {
     var firstname: String?
     var lastname: String?
-    var city: String?
+    var gender: String?
+    var address: address?
     var email: String?
     var password: String?  //TODO: Note. Passwords are string for temporary purposes only
                            //In production application, with actual users, never
@@ -18,4 +19,17 @@ struct User {
     var employee: Bool?
     var rating: Int?
     var employeeDescription: String?
+    var uid: String?
+    var dateOfBirth: String?
+}
+
+extension User {
+    struct address {
+        var city: String?
+        var country: String?
+        var phone: String?
+        var state: String?
+        var streetAddress: String?
+        var zipCode: String?
+    }
 }
