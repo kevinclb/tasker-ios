6ff086ca557bd8baf9871b4531dac8740b873b00
kevinbabou <kevincbabou@gmail.com>
2022-04-14 18:32:20 -0700

    creating a task object for each row for amir to complete rate and pay features.

--- tasker/tasker/ViewControllers/RecentTaskViewController/RecentTasksTableViewController.swift
+++ tasker/tasker/ViewControllers/RecentTaskViewController/RecentTasksTableViewController.swift
@@ -10,13 +10,15 @@ import Firebase
 
 class RecentTasksTableViewController: UITableViewController {
     var recentTasks = [Errand]()
-    var names = ["John Doe", "Clark Kent", "Albert Einstein", "Roger Federer", "Sally Mae"]
+    var chosenRow = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let collRef = db.collection("tasks")
         //in a prod app,
         //we would put the user's client ID here instead of searhing by employee ID
+        //is user on employee page or on client page?
         collRef.whereField("employeeID", isEqualTo: "RVJioN7qcdX9G3KCugsNbJMqcJU2").getDocuments { snapshot, error in
             if error != nil {
                 print("error retrieving documents: \(error?.localizedDescription)")
@@ -42,7 +44,7 @@ class RecentTasksTableViewController: UITableViewController {
         }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
@@ -52,25 +54,27 @@ class RecentTasksTableViewController: UITableViewController {
         print("recent tasks count outside closure: ", recentTasks.count)
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return recentTasks.count
     }
-
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.chosenRow = indexPath.row
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "recentTaskCell", for: indexPath) as! RecentTaskTableViewCell
         cell.taskNameLabel.text = recentTasks[indexPath.row].taskDescription
-        cell.taskerNameLabel.text = names[indexPath.row]
-        
+        cell.taskerNameLabel.text = "test name"
+        cell.task = recentTasks[indexPath.row]
         cell.delegate = self
         return cell
     }
@@ -79,66 +83,69 @@ class RecentTasksTableViewController: UITableViewController {
         return nil
     }
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
 
 extension RecentTasksTableViewController: RecentTaskTableViewCellDelegate {
-
-    func didRateButtonPressed() {
-          let transition = CATransition()
-        transition.duration = 0.5
-          transition.type = CATransitionType.push
-        transition.subtype = .fromRight
-          transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
-          view.window!.layer.add(transition, forKey: kCATransition)
-
-          // this code here is to make the viewcontroller we're presenting and make it show full screen then present it
-          let rateVC = RateUserViewController()
-          rateVC.modalPresentationStyle = .fullScreen
-          // the app will automatically know how to animate the presentation, it will use the transition we made above on its own so that's why we set animated to false
-          present(rateVC, animated: false, completion: nil)
-
-      }
+    
+    func didRateButtonPressed(_ recentTaskCell: RecentTaskTableViewCell, taskButtonTappedFor: String) {
+        print("chosen task: ", recentTaskCell.task?.taskDescription ?? "EMPTY TASK DESCRIPTION")
+        //Here, Amir, you can tap into recentTaskCell.task's fields (employeeID, clientID, etc, and get whatever you need, then perform the transition below.)
+        
+        
+//        let transition = CATransition()
+//        transition.duration = 0.5
+//        transition.type = CATransitionType.push
+//        transition.subtype = .fromRight
+//        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+//        view.window!.layer.add(transition, forKey: kCATransition)
+//        // this code here is to make the viewcontroller we're presenting and make it show full screen then present it
+//        let rateVC = RateUserViewController()
+//        rateVC.modalPresentationStyle = .fullScreen
+//        // the app will automatically know how to animate the presentation, it will use the transition we made above on its own so that's why we set animated to false
+//        present(rateVC, animated: false, completion: nil)
+        
+    }
 }
