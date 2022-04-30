46d95b717f086d61b10139335ad33a8e956b02d1
j3ffr3yv <44720313+j3ffr3yv@users.noreply.github.com>
2022-04-29 21:58:37 -0700

    Messages Back end

--- tasker/tasker/ViewControllers/MessagesViewController/ChatViewController.swift
+++ tasker/tasker/ViewControllers/MessagesViewController/ChatViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ChatViewController: UIViewController, UITableViewDataSource {
     @IBAction func backToConversations(_ sender: Any) {
         
-        let showConv = MessagesViewController()
+        let showConv = RootViewController()
         navigateTo(showConv, .fromRight)
         func navigateTo(_ newViewController: UIViewController, _ transitionFrom:CATransitionSubtype) {
             let transition = CATransition()
@@ -29,29 +29,35 @@ class ChatViewController: UIViewController, UITableViewDataSource {
         }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        print("the count for messages is " + String(messages.count))
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = chatTableView.dequeueReusableCell(withIdentifier: chatCellViewId, for: indexPath) as! ChatCellTableViewCell
-        if (indexPath.row % 2 == 0)
+        if (messages[indexPath.row].sender != Utilities.getUid())
         {
             cell.leftAvatar.isHidden = false
             cell.rightAvatar.isHidden = true
-            cell.chatBubble.alignmentRect(forFrame: CGRect(x:10, y:5, width:80, height:9 ))
             cell.chatBubble.backgroundColor = .gray
+            cell.chatBubble.contentMode = .scaleToFill
+            cell.chatBubble.adjustsFontSizeToFitWidth = true
+            cell.chatBubble.minimumScaleFactor = 0.5
+            cell.chatBubble.numberOfLines = 0
+            cell.chatBubble.text = messages[indexPath.row].body
         }
         else
         {
             cell.leftAvatar.isHidden = true
             cell.rightAvatar.isHidden = false
+            cell.chatBubble.text = messages[indexPath.row].body
             
         }
         
         return cell
     }
     
-
+    var messages = [Message]()
     @IBOutlet weak var messageInput: UITextField!
     @IBOutlet weak var chatTableView: UITableView!
     
--- tasker/tasker/ViewControllers/MessagesViewController/MessagesViewController.swift
+++ tasker/tasker/ViewControllers/MessagesViewController/MessagesViewController.swift
@@ -19,7 +19,7 @@ class MessagesViewController: UIViewController {
         navigateToListTaskVC(newMsgVC, .fromRight)
     }
     
-    var messages = [Message]()
+    var conversations = [Conversation]()
     override func viewDidLoad() {
         super.viewDidLoad()
         uid = Utilities.getUid()
@@ -29,9 +29,39 @@ class MessagesViewController: UIViewController {
         messageViewCollection.dataSource = self
         messageViewCollection.awakeFromNib()
         
-        for i in 1...35{
-            var message1 = Message(body: "Message will go here", sender:User.init())
-            messages.append(message1)
+        let convRef = db.collection("conversations")
+        convRef.whereField("user1ID", isEqualTo: uid).getDocuments { (snapshot, error) in
+            if error != nil {
+                print("error retrieving conversation documents: \(String(describing: error?.localizedDescription))")
+            } else {
+                for d in (snapshot!.documents) {
+                    do {
+                        try self.conversations.append(d.data(as: Conversation.self)!)
+                    } catch {
+                        print("error decoding conversation(errand) document")
+                    }
+                }
+//                DispatchQueue.main.async {
+//                    self.messageViewCollection.reloadData()
+//                }
+            }
+        }
+        
+        convRef.whereField("user2ID", isEqualTo: uid).getDocuments { (snapshot, error) in
+            if error != nil {
+                print("error retrieving conversation documents: \(String(describing: error?.localizedDescription))")
+            } else {
+                for d in (snapshot!.documents) {
+                    do {
+                        try self.conversations.append(d.data(as: Conversation.self)!)
+                    } catch {
+                        print("error decoding conversation(errand) document")
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.messageViewCollection.reloadData()
+                }
+            }
         }
         
     }
@@ -56,20 +86,24 @@ class MessagesViewController: UIViewController {
 extension MessagesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 35
+        return conversations.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: messageCollectionViewCellId, for: indexPath) as! MessageViewCell
-        let message = messages[indexPath.row]
-        cell.lbName.text = message.sender.firstname
-        cell.lbMessage.text = message.body
+        let convo = conversations[indexPath.row]
+        let lastMessageCount = convo.messages.count
+        cell.lbName.text = "Name will go here"
+        cell.lbMessage.text = convo.messages[lastMessageCount-1].body
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(indexPath.row)
         let showChatVC = ChatViewController()
+        DispatchQueue.main.async {
+            showChatVC.messages = self.conversations[indexPath.row].messages
+        }
         navigateTo(showChatVC, .fromRight)
         func navigateTo(_ newViewController: UIViewController, _ transitionFrom:CATransitionSubtype) {
             let transition = CATransition()
