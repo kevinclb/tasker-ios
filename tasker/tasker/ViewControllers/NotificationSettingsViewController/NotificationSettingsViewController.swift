8dd5492966d5250f44239baa38b42dc8e9ad1065
Issa Issa <iissa3339@gmail.com>
2022-05-02 17:59:05 -0700

    Added notif settings vc

---
+++ tasker/tasker/ViewControllers/NotificationSettingsViewController/NotificationSettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationSettingsViewController.swift
+//  tasker
+//
+//  Created by User on 5/2/22.
+//
+
+import UIKit
+
+class NotificationSettingsViewController: UIViewController {
+    @IBOutlet weak var toggle: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func switchToggled(_ sender: Any) {
+        if(toggle.isOn){
+            
+        }else{
+            
+        }
+    }
+    
+
+}
