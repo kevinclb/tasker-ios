d9dba297098d869f81c6d7a7f32b3e7fb74fc01c
j3ffr3yv <44720313+j3ffr3yv@users.noreply.github.com>
2022-05-07 20:54:01 -0700

    fixing

--- tasker/tasker/ViewControllers/MessagesViewController/NewMessageViewController.swift
+++ tasker/tasker/ViewControllers/MessagesViewController/NewMessageViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jeffrey Viramontes on 3/31/22.
 //
-
 import UIKit
 import Firebase
 
@@ -32,10 +31,7 @@ class NewMessageViewController: UIViewController {
         super.viewDidLoad()
         var users = [Utilities.getUid(), sendToID]
         usersSorted = users.sorted{$0.localizedCompare($1) == .orderedAscending}
-<<<<<<< HEAD
         print("user 1 " + users[0] + " User 2 " + users[1])
-=======
->>>>>>> main
             db.collection("conversations").whereField("user1ID", isEqualTo: usersSorted[0]).whereField("user2ID", isEqualTo: usersSorted[1]).getDocuments() { (querySnapshot, error) in
                 if let querySnapshot = querySnapshot {
                     for document in querySnapshot.documents {
@@ -51,17 +47,9 @@ class NewMessageViewController: UIViewController {
         messageToSend = messageField.text!
         if(docID == "")
         {
-<<<<<<< HEAD
             convo = Conversation(user1ID: usersSorted[0], user2ID: usersSorted[1], messages: [Message(body: messageToSend, sender: Utilities.getUid())])
             do {
                print(try db.collection("conversations").addDocument(from: self.convo.self))
-=======
-            convo = Conversation(user1ID: users[0], user2ID: users[1], messages: [Message(body: messageToSend, sender: Utilities.getUid())])
-            do {
-               print(try db.collection("conversations").addDocument(from: self.convo.self))
-                let backToMsgsVC = MessagesViewController()
-                navigateToListTaskVC(backToMsgsVC, .fromRight)
->>>>>>> main
             }catch{
                 print("Error adding document")
             }
@@ -71,12 +59,7 @@ class NewMessageViewController: UIViewController {
             let newMessage = ["body": messageToSend, "sender": Utilities.getUid()]
             db.collection("conversations").document(docID).updateData(["messages": FieldValue.arrayUnion([newMessage])])
         }
-<<<<<<< HEAD
         
-=======
-    }
-    func navigateToListTaskVC(_ newViewController: UIViewController, _ transitionFrom:CATransitionSubtype) {
->>>>>>> main
         let transition = CATransition()
         transition.duration = 0.5
         transition.type = CATransitionType.push
@@ -85,14 +68,4 @@ class NewMessageViewController: UIViewController {
         view.window!.layer.add(transition, forKey: kCATransition)
         dismiss(animated: false, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
